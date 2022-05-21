adedee8d46a658f6289a1741fc6d41f549dc0d5c
Stukotolog <stukotolog@mail.ru>
2022-05-21 17:30:23 +0300

    fixed calculations

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,10 @@
 public class CreditPaymentService {
-    public double calculate(int kp) {
+    public double calculate(int numberofmonths) {
         int sk = 1_000_000; //сумма кредита
         double epsp = 9.99; //годовая ставка в процентах//
-        double mps = epsp / kp / 100; //месячная процентная ставка
+        double mps = epsp / 12 / 100; //месячная процентная ставка
         double y = mps + 1;//значение для рассчета в формуле
-        double x = Math.pow(y, kp);//значение для рассчета в формуле
+        double x = Math.pow(y, numberofmonths);//значение для рассчета в формуле
         double ap = sk * (mps + (mps / (x - 1))); //аннуитетный платеж
         return ap;
 
