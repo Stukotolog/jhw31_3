8bfceb6096b975a56c19c6628195a09b554ecf61
Stukotolog <stukotolog@mail.ru>
2022-05-21 18:22:58 +0300

    fixed names again

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,11 +1,11 @@
 public class CreditPaymentService {
     public double calculate(int numberofmonths) {
-        int sk = 1_000_000; //сумма кредита
-        double epsp = 9.99; //годовая ставка в процентах//
-        double mps = epsp / 12 / 100; //месячная процентная ставка
-        double y = mps + 1;//значение для рассчета в формуле
+        int loan = 1_000_000; //сумма кредита
+        double percent_year = 9.99; //годовая ставка в процентах//
+        double percent_month = percent_year / 12 / 100; //месячная процентная ставка
+        double y = percent_month + 1;//значение для рассчета в формуле
         double x = Math.pow(y, numberofmonths);//значение для рассчета в формуле
-        double ap = sk * (mps + (mps / (x - 1))); //аннуитетный платеж
+        double ap = loan * (percent_month + (percent_month / (x - 1))); //аннуитетный платеж
         return ap;
 
     }
